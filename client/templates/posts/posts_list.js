d59cbebfdefca2126b00097ac8e4c8dfa5e1f1b7
Sunave <antti.suniala@gmail.com>
2015-06-03 14:56:30 +0300

    Animate post reordering when voting

---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,39 @@
+Template.postsList.onRendered(function() {
+  this.find('.wrapper')._uihooks = {
+    moveElement: function (node, next) {
+      // Get baseline
+      var $node = $(node), $next = $(next);
+      var oldTop = $node.offset().top;
+      var height = $node.outerHeight(true);
+
+      // Find all elements between next and node
+      var $inBetween = $next.nextUntil(node);
+      if ($inBetween.length === 0) {
+        $inBetween = $node.nextUntil(next);
+      }
+
+      // Put node in place
+      $node.insertBefore(next);
+
+      // Measure new top
+      var newTop = $node.offset().top;
+
+      // Move node back to baseline
+      $node
+          .removeClass('animate')
+          .css('top', oldTop - newTop);
+
+      // Push every other element down or up to put them back
+      $inBetween
+          .removeClass('animate')
+          .css('top', oldTop < newTop ? height : -1 * height);
+
+      // Force redraw
+      $node.offset();
+
+      // Animate, then move to final position
+      $node.addClass('animate').css('top', 0);
+      $inBetween.addClass('animate').css('top', 0);
+    }
+  }
+});
